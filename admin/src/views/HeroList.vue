51139cbda991afff19a95e8b7dbd6e55ae792a18
unknown <1329298050@qq.com>
2020-02-10 20:31:44 +0800

    p21-finished

--- admin/src/views/HeroList.vue
+++ admin/src/views/HeroList.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="about">
-    <h1>分类列表</h1>
+    <h1>英雄列表</h1>
      <el-table :data="items">
         <el-table-column prop="_id" label="ID" width="220">
         </el-table-column>
-        <el-table-column prop="parent.name" label="上级分类" width="220">
+        <el-table-column prop="name" label="英雄名称">
         </el-table-column>
-        <el-table-column prop="name" label="分类名称">
+        <el-table-column prop="title" label="称号">
+        </el-table-column>
+        <el-table-column prop="avater" label="头像">
+          <template slot-scope="scope">
+            <img :src="scope.row.avater" style="height:3rem">
+          </template>
         </el-table-column>
          <el-table-column
       fixed="right"
       label="操作"
       width="100">
       <template slot-scope="scope" >
-        <el-button type="text" size="small" @click="$router.push(`/categories/edit/${scope.row._id}`)">编辑</el-button>
+        <el-button type="text" size="small" @click="$router.push(`/heroes/edit/${scope.row._id}`)">编辑</el-button>
         <el-button type="text" size="small" @click="remove(scope.row)"  >删除</el-button>
       </template>
     </el-table-column>
@@ -30,7 +35,7 @@ export default {
   },
   methods:{
     async fetch(){
-      const res = await this.$http.get('rest/categories')
+      const res = await this.$http.get('rest/heros')
       this.items = res.data;
     },
     async remove(row){
@@ -39,8 +44,8 @@ export default {
           cancelButtonText: '取消',
           type: 'warning'
         }).then(async() => {
-          // const res = await this.$http.delete(`categories/${row._id}`)
-          await this.$http.delete(`rest/categories/${row._id}`)
+          // const res = await this.$http.delete(`items/${row._id}`)
+          await this.$http.delete(`rest/heros/${row._id}`)
           this.$message({
             type: 'success',
             message: '删除成功!'
